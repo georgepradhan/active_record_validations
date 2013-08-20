004c02d55902b14919d27c38be21ee124fe9d2f7
George Pradhan <kgeorgepradhan@gmail.com>
2013-08-19 23:30:22 -0700

    add validations to models, pseudocode for error handling in routes controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,9 +9,18 @@ get '/events/:id/show' do |id|
 end
 
 get '/events/new' do
-  #TODO IMPLEMENT ME
+  # route to a view that has a form for creating
+  # an event
 end
 
 post '/events/create' do
-  #TODO IMPLEMENT ME
+  # this is the route triggered by submitting
+  # a form on the /events/new view
+  # this is where the validations are checked
+  # and where the errors.full_messages
+  # handling needs to go before redirecting 
+  # or rendering some other view of the 
+  # created event (redirect to id/show...),
+  # which itself is wehre the errors will be 
+  # displayed if there are any
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,23 @@
 class Event < ActiveRecord::Base
+
+  # a. The events date is empty, in the past, or is not valid.   
+  validates :date, presence: true
+  validate :date_not_in_past
+  # validates :date, #is_valid
+  
+  # b. The events title is already taken or empty. 
+  validates :title, uniqueness: true
+  validates :title, presence: true
+
+  # c. The event organizers name is empty. 
+  validates :organizer_name, presence: true
+
+  # d. The event organizers email address is invalid.
+  validates :organizer_email, format: { with: =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
+  def date_not_in_past
+    if :date < Date.today
+      errors.add(:date, "is in the past")
+    end
+  end
 end
