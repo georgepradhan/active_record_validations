454cc6d27b53f9c1c6099c05b35c6dce83f2ae69
George Pradhan <kgeorgepradhan@gmail.com>
2013-08-20 10:17:18 -0700

    updated event model for date in past validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,9 +2,9 @@ class Event < ActiveRecord::Base
 
   # a. The events date is empty, in the past, or is not valid.   
   validates :date, presence: true
-  validate :date_not_in_past
-  # validates :date, #is_valid
-  
+  validate :foo_not_in_past # can be named whatever
+  # validate :date_is_valid -- html takes care of this
+
   # b. The events title is already taken or empty. 
   validates :title, uniqueness: true
   validates :title, presence: true
@@ -13,11 +13,15 @@ class Event < ActiveRecord::Base
   validates :organizer_name, presence: true
 
   # d. The event organizers email address is invalid.
-  validates :organizer_email, format: { with: =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :organizer_email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 
-  def date_not_in_past
-    if :date < Date.today
-      errors.add(:date, "is in the past")
+  def foo_not_in_past
+    # date (not in the form of a :symbol)
+    # is an instance variable (and getter method)
+    # that refers to the date of the Event object...
+    if self.date < Date.today
+      errors.add(:foo, "is in the past") # can add whatever to errors
     end
   end
+
 end
